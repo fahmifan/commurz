265f03939d68bd1897b9bfffada6d16a8200b00f
fahmifan <me@fahmifan.id>
2023-07-12 11:35:05 +0700

    feat: update

--- pkg/service/service_dbtrx.go
+++ pkg/service/service_dbtrx.go
@@ -3,6 +3,7 @@ package service
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 )
 
 func Transaction(db *sql.DB, fn func(*sql.Tx) error) error {
@@ -11,14 +12,17 @@ func Transaction(db *sql.DB, fn func(*sql.Tx) error) error {
 		return err
 	}
 
-	if err := fn(tx); err != nil {
-		if txErr := tx.Rollback(); txErr != nil {
-			return txErr
-		}
-		return err
+	txErr := fn(tx)
+	// success
+	if txErr == nil {
+		return tx.Commit()
 	}
 
-	return tx.Commit()
+	if err := tx.Rollback(); err != nil {
+		return fmt.Errorf("txErr: %w: rollabck err: %w", txErr, err)
+	}
+
+	return txErr
 }
 
 func isNotFoundErr(err error) bool {
